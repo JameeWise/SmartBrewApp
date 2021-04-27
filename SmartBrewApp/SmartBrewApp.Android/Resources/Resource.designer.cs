1636788cf59d6df24eebdaa65208bb9e4c52fa15
Luke Holt <lukeholt2@protonmail.com>
2021-04-27 11:13:40 -0400

    hopefully fixes network issues

--- SmartBrewApp/SmartBrewApp.Android/Resources/Resource.designer.cs
+++ SmartBrewApp/SmartBrewApp.Android/Resources/Resource.designer.cs
@@ -14040,7 +14040,10 @@ namespace SmartBrewApp.Droid
 		{
 			
 			// aapt resource value: 0x7F100000
-			public const int xamarin_essentials_fileprovider_file_paths = 2131755008;
+			public const int network_security_config = 2131755008;
+			
+			// aapt resource value: 0x7F100001
+			public const int xamarin_essentials_fileprovider_file_paths = 2131755009;
 			
 			static Xml()
 			{
